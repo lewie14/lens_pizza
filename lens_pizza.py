ea68f2fab92db153d4ca11b61d0df1783f336638
lewie14 <lewie@netvigator.com>
2018-06-04 10:07:49 +0800

    Calculate number of pizzas

--- lens_pizza.py
+++ lens_pizza.py
@@ -1,3 +1,9 @@
 #Set up lists for toppings and prices
 toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
 prices = [2, 6, 1, 3, 2, 7, 2]
+
+#Number of pizzas is the length of the toppings list
+num_pizzas = len(toppings)
+
+#let user know how many types of pizza
+print("We sell " + str(num_pizzas) + "different kinds of pizza!")
